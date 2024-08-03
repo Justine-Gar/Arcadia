6d0480098d9736b7b7acc0644f5cf753fc247503
JustinG <jg.garandel@hotmail.fr>
2024-08-03 12:10:24 +0200

    add file src\utils with passwordHasher.php

---
+++ Src/utils/PasswordHasher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src\utils;
+
+class PasswordHasher {
+
+  public function hashPassword($password) {
+      $options = [
+          'memory_cost' => 65536, // 64 MiB
+          'time_cost' => 4,
+          'threads' => 3
+      ];
+      return password_hash($password, PASSWORD_ARGON2ID, $options);
+  }
+
+  public function verifyPassword($password, $hash) {
+      return password_verify($password, $hash);
+  }
+}
