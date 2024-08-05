f5d472a1625346377ef5347aa0baaf616ca1eae2
JustinG <jg.garandel@hotmail.fr>
2024-08-05 14:25:53 +0200

    update file PassWordHasher.php

--- Src/utils/PasswordHasher.php
+++ Src/utils/PasswordHasher.php
@@ -2,18 +2,20 @@
 
 namespace src\utils;
 
-class PasswordHasher {
+class PasswordHasher
+{
+    public function hashPassword($password)
+    {
+        $options = [
+            'memory_cost' => 65536, // 64 MiB
+            'time_cost' => 4,
+            'threads' => 3
+        ];
+        return password_hash($password, PASSWORD_ARGON2ID, $options);
+    }
 
-  public function hashPassword($password) {
-      $options = [
-          'memory_cost' => 65536, // 64 MiB
-          'time_cost' => 4,
-          'threads' => 3
-      ];
-      return password_hash($password, PASSWORD_ARGON2ID, $options);
-  }
-
-  public function verifyPassword($password, $hash) {
-      return password_verify($password, $hash);
-  }
+    public function verifyPassword($password, $hash)
+    {
+        return password_verify($password, $hash);
+    }
 }
