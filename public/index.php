2472cc1e5918e4fc0e7b7870faf0e7591af8ebc0
JustinG <jg.garandel@hotmail.fr>
2024-08-12 15:57:50 +0200

    Config .env witch database

--- public/index.php
+++ public/index.php
@@ -1,20 +1,43 @@
 <?php
+//Chemin d'acces
+define('BASE_PATH', dirname(__DIR__));
 
-require_once '../core/Autoloader.php';
-core\Autoloader::register(__DIR__ . '/..');
+//Charger les chemin
+require_once BASE_PATH . '/lib/core/Autoloader.php';
+require_once BASE_PATH . '/lib/core/EnvLoader.php';
+//Initialiser
+lib\core\Autoloader::register();
+lib\core\EnvLoader::load(BASE_PATH . '/.env');
 
-use core\Router;
 
-$router = new core\Router();
+//Connection a la BDD
+$dbConnection = lib\config\Database::getConnection();
 
-// Définition des routes
-$router->addRoute('/', 'Src\controllers\HomeController', 'index');
-$router->addRoute('/users', 'Src\controllers\UserController', 'list');
+// Charger la configuration en fonction de l'environnement
+//getenv() recupere les variable d'envirronement (juste changer dans .env la variable APP_ENV)
+$config = require BASE_PATH . '/lib/config/' . getenv('APP_ENV') . '.php';
 
-// Récupération de l'URL demandée
-$url = $_SERVER['REQUEST_URI'];
+//Charger les routes
+$routes = require BASE_PATH . '/lib/config/routes.php';
 
-// Dispatch de la requête
-$router->dispatch($url);
+//Initialiser le Router
+$router = new Lib\core\Router($routes);
+
+
+// Gérer la requête
+$url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$url = str_replace('/arcadia', '', $url);
+
+try {
+  $router->handleRequest($url);
+
+} catch (Exception $e)
+{
+  //Gérer les erreurs
+  $response = new lib\core\Responce();
+  $response->setStatusCode(500);
+  $response->json(['error' => 'Une erreur interne est survenue']);
+  $response->send();
+}
 ?>
 
